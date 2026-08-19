107a60ca06f94c5661ab03381fb03ce684616641
Andrew Witkowski <andrewwitkowski007@gmail.com>
2026-08-19 11:44:03 -0500

    Updated to add env file with API key for security.

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,9 +1,17 @@
 export class AuthService {
 
   static headers() {
+
+    const apiKey = process.env.REQRES_API_KEY;
+
+    if (!apiKey) {
+      throw new Error('REQRES_API_KEY is not defined in the environment');
+    }
+
     return {
-      'x-api-key': '***REMOVED***'
+      'x-api-key': apiKey
     };
+
   }
 
 }
